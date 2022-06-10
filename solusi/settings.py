9ff9e722cddd44908e9c2225e4afd25da1bd23ee
sizah <sizahncube>
2022-06-10 09:14:12 +0200

    my solusi live

--- solusi/settings.py
+++ solusi/settings.py
@@ -144,12 +144,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
-
-STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'solusiweb/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ROOT = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(ROOT, 'static')
+#ROOT = os.path.dirname(BASE_DIR)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
